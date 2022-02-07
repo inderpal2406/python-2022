f47778896966ea54c5230612aeb5d4f279f11b30
Inderpal Singh <inderpal2406@gmail.com>
2022-02-07 09:12:46 +0530

    Add 27-machine-info.py.

---
+++ 27-machine-info.py
@@ -0,0 +1,22 @@
+# This script will print the machine info on which this script is executed.
+
+# Import modules.
+
+import clear_screen_module
+import platform
+
+# Clear the screen.
+
+clear_screen_module.clear_screen()
+
+# Print purpose of the script.
+
+print(f"This script will display the machine information.\n")
+
+# Print the machine information.
+
+print(f"Machine hostname: {platform.node()}")
+print(f"Operating system: {platform.system()}")
+print(f"Operating system version: {platform.platform()}")
+print(f"Processor information: {platform.processor()}")
+print(f"Processor architecture: {platform.architecture()[0]}\n")
