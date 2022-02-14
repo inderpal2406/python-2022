4a468b1fb27e71b8f410ffaf35b21e864accf106
Inderpal Singh <inderpal2406@gmail.com>
2022-02-14 17:21:45 +0530

    Add 39-simple-display-function.py.

---
+++ 39-simple-display-function.py
@@ -0,0 +1,12 @@
+# Simple script with a simple function.
+
+# Define functions.
+
+def display():
+    print(f"Hello from the function.")
+    print(f"How are you? from the function.")
+    return None
+
+# Call the function.
+
+display()
