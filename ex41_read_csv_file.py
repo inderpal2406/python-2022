55084e083a7359b6a836ceb20586006668490a45
Inderpal Singh <inderpal2406@gmail.com>
2022-06-18 18:32:37 +0530

    Update ex41_read_csv_file.py

--- ex41_read_csv_file.py
+++ ex41_read_csv_file.py
@@ -2,10 +2,23 @@
 
 import csv
 
+# Read CSV file with default limiter comma.
+
 file = "csv-files\\file1.csv"
 fileobj = open(file,"r")
 content = csv.reader(fileobj,delimiter=",")
 #print(f"{type(content)}")
+print("\nContent of file1.csv with delimiter as , is:")
+for each in content:
+    print(f"{each}")
+fileobj.close()
+
+# Read CSV file with | as delimiter.
+
+file = "csv-files\\file2.csv"
+fileobj = open(file,"r")
+content = csv.reader(fileobj,delimiter="|")
+print("\nContent of file2.csv with delimiter as | is:")
 for each in content:
     print(f"{each}")
 fileobj.close()
